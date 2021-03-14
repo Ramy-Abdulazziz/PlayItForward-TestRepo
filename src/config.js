1c3ee7b4d83cb93625f1bf626be1680a6e8e4f78
Ramy-Abdulazziz <Ramy.abdulazziz1@gmail.com>
2021-03-14 15:24:42 -0400

    Initial Project Version

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyAZNxQXIs3Mgp1eTin3QcFgjCRurgEpVAE",
+        authDomain: "play-it-forward-718c3.firebaseapp.com",
+        projectId: "play-it-forward-718c3",
+        storageBucket: "play-it-forward-718c3.appspot.com",
+        messagingSenderId: "220126010769",
+        appId: "1:220126010769:web:cd800d001371fab8aace30",
+        measurementId: "G-7MVP1NW6GX"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+   
+
